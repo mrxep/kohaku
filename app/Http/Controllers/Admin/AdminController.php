8fcbbb486c16e6b3f3680db6c608b7336f1a90b1
mrxep <48308459+mrxep@users.noreply.github.com>
2020-03-10 16:23:40 +0700

    push

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function __Controller()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+}
